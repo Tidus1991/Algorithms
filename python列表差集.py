26fb181d50877870f42f35a67c10c3bcd0a9a962
Tidus1991 <j554158979@gmail.com>
2017-09-17 15:42:04 +0900

    Update python列表差集.py

--- python列表差集.py
+++ python列表差集.py
@@ -6,6 +6,6 @@ This is a temporary script file.
 """
 class Solution(object):
     def findDisappearedNumbers(self, nums):
-        x=[i for i in range (1,len(nums)+1)]
+        x = [i for i in range (1,len(nums) + 1)]
         diff = list(set(x).difference(set(nums)))
         return diff
