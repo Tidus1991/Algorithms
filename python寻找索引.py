14c4ee0e193259ffe550ea5a6a4d1528f03d76eb
Tidus1991 <j554158979@gmail.com>
2017-09-22 10:30:38 +0900

    Update python寻找索引.py

--- python寻找索引.py
+++ python寻找索引.py
@@ -4,8 +4,6 @@ Created on Fri Sep 22 09:39:05 2017
 
 @author: Tidus
 """
-nums = [4,7,9,12]
-target = 8
 
 class Solution(object):
     def searchInsert(self, nums, target):
@@ -21,11 +19,10 @@ class Solution(object):
             nums.sort()
             return nums.index(target)
         
-f = Solution()
-print(f.searchInsert(nums,target))
+
 
 '''
+the other method
 return len([x for x in nums if x<target])
 
-返回小于target的值的长度，长知识了。
 '''
