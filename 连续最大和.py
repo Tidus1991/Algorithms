b079a4d07885bc406f5f59c0d55580d33f3c8529
Tidus1991 <j554158979@gmail.com>
2017-09-25 15:26:09 +0900

    Add files via upload

---
+++ 连续最大和.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 15:12:16 2017
+
+@author: Tidus
+"""
+
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    # 6:57
+    def maxSubArray(self, A):
+        if not A:
+            return 0
+
+        curSum = maxSum = A[0]
+        for num in A[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+
+        return maxSum
