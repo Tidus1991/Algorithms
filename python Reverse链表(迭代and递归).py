3293be8c837b0ddce1880b9d268ae9c2df015c0f
Tidus1991 <j554158979@gmail.com>
2017-10-15 20:08:28 +0800

    Add files via upload

---
+++ python Reverse链表(迭代and递归).py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 14 16:45:32 2017
+
+@author: Tidus
+"""
+
+class Solution:
+# @param {ListNode} head
+# @return {ListNode}
+    def reverseList(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+    
+
+class Solution2:
+# @param {ListNode} head
+# @return {ListNode}
+    def reverseList(self, head):
+        return self._reverse(head)
+    
+    def _reverse(self, node, prev=None):
+        if not node:
+            return prev
+        n = node.next
+        node.next = prev
+        return self._reverse(n, node)
